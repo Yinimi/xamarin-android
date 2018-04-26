1b738232dc6c61d2305d8a141fe22b0a27ec7e60
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-04-26 17:14:11 -0500

    WIP Embedding targets

---
+++ src/Xamarin.Android.Build.Tasks/Tests/Xamarin.Android.Build.Tests/EmbeddinatorTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Xamarin.ProjectTools;
+
+namespace Xamarin.Android.Build.Tests
+{
+	public class EmbeddinatorTest : BaseTest
+    {
+		[Test]
+		public void GenerateJavaStubs ()
+		{
+			var proj = new XamarinAndroidApplicationProject ();
+			proj.Imports.Add (new Import (() => @"$(MSBuildExtensionsPath)\Xamarin\Android\Xamarin.Android.Embedding.targets"));
+			using (var b = CreateApkBuilder ($"temp/{TestContext.CurrentContext.Test.Name}")) {
+				b.Target = "GenerateEmbeddedJavaStubs";
+				Assert.IsTrue (b.Build (proj), "Build should have succeeded.");
+			}
+		}
+	}
+}
